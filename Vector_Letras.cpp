46ca4eabc3971402fffb4c5101ffbd6569e0be96
Mastantonto <lazaro.esart18@zaragoza.salesianos.edu>
2026-02-17 11:17:56 +0100

    Add files via upload

---
+++ Vector_Letras.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+	int cont;
+	char letras[4]; //vector para guardar letras
+//	letras[0]='H';
+//	letras[1]='O';
+//	letras[2]='L';
+//	letras[3]='A';
+	for(cont=0;cont<=3;cont++){//for cont in range(0,4):
+	//cont ++ es lo mismo que cont=cont+1
+		printf("\nLetra[%i]= ",cont);
+		scanf(" %c",&letra[cont]);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	return(0);
+}
